e3857a79c6df4cac2cd14d4fc1bb5b78661382fc
hanaaaboush-droid <hanaaaboush@gmail.com>
2026-08-27 19:12:50 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        addNewsPanel.classList.add("active");
+        addNewsPanel.classList.add("show");
 
     }
 
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (!addNewsPanel) return;
 
-        addNewsPanel.classList.remove("active");
+        addNewsPanel.classList.remove("show");
 
         if (newsForm) {
             newsForm.reset();
